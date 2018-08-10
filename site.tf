eec184e2f1264a69e831cefeec90908965da177f
Fan Yang <youngf@gmail.com>
2018-08-10 11:56:12 +1000

    updated role id

--- site.tf
+++ site.tf
@@ -141,8 +141,8 @@ resource "aws_autoscaling_attachment" "test_autoscaling_attachment" {
   autoscaling_group_name = "${aws_autoscaling_group.test_asg.id}"
 }
 
-resource "aws_iam_role" "test_role" {
-  name = "test-role"
+resource "aws_iam_role" "test_iam_role" {
+  name = "test-iam-role"
   assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -160,12 +160,12 @@ EOF
 
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test-profile"
-  role = "${aws_iam_role.test_role.name}"
+  role = "${aws_iam_role.test_iam_role.name}"
 }
 
 resource "aws_iam_role_policy" "test_policy" {
   name = "test-policy"
-  role = "${aws_iam_role.test_role.id}"
+  role = "${aws_iam_role.test_iam_role.id}"
   policy = <<EOF
 {
     "Version": "2012-10-17",
